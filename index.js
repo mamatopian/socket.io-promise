27bf3423facaebd1f1dc97348350e6f5bd97a66f
Amir Mamaghani <mamaghani@biosdesign.eu>
2015-01-06 23:26:30 +0100

    added emit method

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 module.exports = function (Socket) {
 
-    Socket.prototype.givePromise = function(event, callback, timeout) {
+    Socket.prototype.givePromise = function(event, args, callback, timeout) {
         var self = this;
 
         var timedOutOrEventEmitted = function(data){
             self.removeListener(event, timedOutOrEventEmitted);
             callback(data, self);
-            this.promiseDelay = clearTimeout(self.promiseDelay);
+            self.promiseDelay = clearTimeout(self.promiseDelay);
         };
+        this.emit(event, args);
         this.on(event, timedOutOrEventEmitted);
         this.promiseDelay = setTimeout(timedOutOrEventEmitted, timeout);
     };
