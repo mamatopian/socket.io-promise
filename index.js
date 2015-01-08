624517c2fa273a1a8e3add11565934b4f78d7a77
Amir Mamaghani <mamaghani@biosdesign.eu>
2015-01-08 03:28:53 +0100

    promise needs special ID in case you are binding it to multimple calls

--- index.js
+++ index.js
@@ -2,16 +2,24 @@ module.exports = function (Socket) {
 
     Socket.prototype.giveme = function(event, callback, args, timeout) {
         var self = this;
-        var id = Math.random().toString().replace('.','');
+        var requestId = Math.random().toString().replace('.','');
+        if(typeof args === 'string'){
+            args = {data:args};
+        }
+        args.requestId = requestId;
 
         var timedOutOrEventEmitted = function(data){
-            self.removeListener(event, timedOutOrEventEmitted);
-            delete data.requestId;
-            callback(data, self);
-            self.promiseDelay = clearTimeout(self.promiseDelay);
+            if(data.requestId == requestId){
+                self.removeListener(event, timedOutOrEventEmitted);
+                delete data.requestId;
+                self.promiseDelay = clearTimeout(self.promiseDelay);
+                callback(data, self);
+            }
         };
         this.emit(event, args);
         this.on(event, timedOutOrEventEmitted);
-        this.promiseDelay = setTimeout(timedOutOrEventEmitted, timeout);
+        this.promiseDelay = setTimeout(function(){
+            timedOutOrEventEmitted({ data: { requestId: requestId } });
+        }, timeout);
     };
 };
