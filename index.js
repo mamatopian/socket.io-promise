556d7439bc38c44ab9cc3e83c2caea8e3e7f7b4a
Amir Mamaghani <mamaghani@biosdesign.eu>
2015-01-09 14:10:32 +0100

    added hashing request id

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 module.exports = function (Socket) {
 
+    var crypto = require('crypto');
+
     Socket.prototype.giveme = function(event, callback, args, timeout) {
-        var self = this;
-        var requestId = Math.random().toString().replace('.','');
+        var md5sum = crypto.createHash('md5');
+        var self = this;    
+        var requestId = md5sum.update(event + JSON.stringify(args) + timeout.toString() + JSON.stringify(callback)).digest('hex');
         if(typeof args === 'string'){
             args = {data:args};
+        }else if ( args instanceof Array ){
+            args = {data: args};
         }
+        if(typeof self.promiseDelay === 'undefined')
+            self.promiseDelay = {};
         args.requestId = requestId;
 
         var timedOutOrEventEmitted = function(data){
             if(data.requestId == requestId){
                 self.removeListener(event, timedOutOrEventEmitted);
+                self.promiseDelay[requestId] = clearTimeout(self.promiseDelay);
                 delete data.requestId;
-                self.promiseDelay = clearTimeout(self.promiseDelay);
+                delete self.promiseDelay[requestId];
                 callback(data, self);
             }
         };
         this.emit(event, args);
         this.on(event, timedOutOrEventEmitted);
-        this.promiseDelay = setTimeout(function(){
+        self.promiseDelay[requestId] = setTimeout(function(){
             timedOutOrEventEmitted({ data: { requestId: requestId } });
         }, timeout);
     };
